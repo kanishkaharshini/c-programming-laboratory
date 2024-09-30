c26086d58bbe7e73c478c890ba56ebd98e0db78c
kanishkaharshini <kanishkaharshini20@gmail.com>
2024-09-30 15:00:51 +0530

    Create add.c

---
+++ add.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    printf("a: ");
+    scanf("%d",&a);
+    printf("b: ");
+    scanf("%d",&b);
+    int sum=a+b;
+    printf("sum:%d",sum);
+    return 0;
+}
